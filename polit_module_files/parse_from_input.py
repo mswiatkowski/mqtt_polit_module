3f2da15fcc4a754e82a8d6f689aeca1c63fbc9e5
Marcin Świątkowski <martinswiat@gmail.com>
2022-02-15 22:18:17 +0100

    Add files via upload

--- polit_module_files/parse_from_input.py
+++ polit_module_files/parse_from_input.py
@@ -1,5 +1,5 @@
 
-user_input = input("usr_input>>> ")
+user_input = input("usr_input>>> ").lower()
 split_usr_input = user_input.split()
 print(split_usr_input)
 
@@ -17,7 +17,8 @@ słowo klucz: lista inputów użytkownika
 questions_keys = {
     "kto": ["kto", "któż", "którzy"],
     "ile": ["ile", "ileż"],
-    "ilu": ["ilu", "iluż"]
+    "ilu": ["ilu", "iluż"],
+    "w": ["w"]
 }
 
 time_keys = {
@@ -29,24 +30,42 @@ time_keys = {
 occupations_keys = {
     "prezydent": ["prezydent", "prezydentem", "prezydentów"],
     "posel": ["poseł", "posłem", "posłów"],
-    "prezes": ["prezes", "prezesem"],
-    "premier": ["premier", "premierem"],
+    "prezes": ["prezes", "prezesem", "prezesów"],
+    "premier": ["premier", "premierem", "premierów"],
     "leader": ["przewodniczący", "przewodniczącym"],
     "rpo": ["rpo", "rzecznik", "rzecznikiem"]
 }
 
+gender_keys = {
+    "k": ["k",
+          "kobieta",
+          "kobiet",
+          "kobiety"],
+    "m": ["m",
+          "mężczyzna",
+          "mężczyzn",
+          "mężczyźni"]
+}
+
 clubs_keys = {
     "pis": ["pis",
+            "prawo i sprawiedliwość",
             "pisu",
             "pisowi",
-            "prawo i sprawiedliwość",
+            "prawa",
+            "prawo",
+            "prawie",
+            "prawu",
             "prawa i sprawiedliwości",
             "prawu i sprawiedliwości",
             "partia rządząca", "partii rządzącej",
             "klub parlamenterny prawo i sprawiedliwość"],
     "ko": ["ko",
-           "koalicja obywatelska"],
+           "koalicja obywatelska",
+           "koalicja",
+           "koalicji"],
     "lewica": ["lewica",
+               "lewica",
                "lewicy",
                "klub poselski lewicy",
                "klubu poselskiego lewicy",
@@ -56,12 +75,14 @@ clubs_keys = {
                "koalicyjnego klubu poselskiego lewicy"],
     "kp": ["kp",
            "koalicja polska",
+           "kape",
            "koalicji polskiej",
            "klub parlamentarny koalicja polska",
            "klubie parlamentarnym koalicji polskiej",
            "klubu parlamentarnego koalicji polskiej",
            "klub parlamentarny koalicji polskiej"],
     "konfederacja": ["konfederacja",
+                     "konfederacja",
                      "konfederacji",
                      "konfa",
                      "konfie",
@@ -70,71 +91,43 @@ clubs_keys = {
                      "kole poselskim konfederacja",
                      "koła poselskiego konfederacja"],
     "polska2050": ["polska 2050",
+                   "polska 2050",
                    "polsce 2050",
+                   "2050",
                    "koło parlamentarne polska 2050"],
     "porozumienie": ["porozumienie",
+                     "porozumienie",
                      "porozumieniu",
                      "porozumienia"],
-    "kukiz15": ["kukiz15"],
+    "kukiz15": ["kukiz",
+                "kukiz15"],
     "ps": ["ps",
-           "p s"
-           "pe es",
            "polskie sprawy",
+           "pees",
+           "sprawy",
+           "spraw",
+           "sprawach",
            "polskich spraw",
            "polskich sprawach",
            "koło poselskie polskie sprawy",
            "kole poselskim polskie sprawy",
            "koła poselskiego polskie sprawy"],
-    "pps": ["pps",
+    "pps": ["pepees",
+            "pps",
             "koło parlamentarne pps",
             "kole parlamentarym pps"],
     "niezrzeszony": ["niezrzeszony",
                      "niezrzeszeni",
-                     "niezrzeszonych"]
+                     "niezrzeszonych"],
 }
 
-questions = {
-    "kto": 'kto',
-    "któż": 'kto',
-    "ile": 'ile',
-    "ilu": 'ile'
-}
-
-time = {
-    "jest": 'jest',
-    "jestże": 'jest',
-    "był": 'był'
-}
-
-occupations = {
-    "prezydent": 'prezydent',
-    "prezydentem": 'prezydent',
-    "poseł": 'poseł',
-    "posłem": 'poseł',
-    "posłów": 'posłowie',
-    "premier": 'premier',
-    "premierem": 'premier',
-    "prezes": 'prezes',
-    "prezesem": 'prezes'
-}
-
-parties = {
-    "pis": 'pis',
-    "pisu": 'pis',
-    "peo": 'po',
-    "razem": 'razem',
-    "konfederacja": 'konfederacja',
-    "konfederacji": 'konfederacja'
-}
-
-
+dicts = [questions_keys, time_keys, occupations_keys, gender_keys, clubs_keys]
 ################################################################################
 
 
 list_of_keys = []
 
 def parsing_from_input(splitted_input):
-    dicts = [questions_keys, time_keys, occupations_keys, clubs_keys]
     for dictionary in dicts:
         for word in splitted_input:
             for key in dictionary.keys():
--- polit_module_files/search_engine.py
+++ polit_module_files/search_engine.py
@@ -7,26 +7,27 @@ keywords = parse_from_input.input_words
 ################################################################################
 
 def search_politicians(keyword):
-    with open('politicians.csv', 'r') as polits:
+    with open('politicians.csv', 'r', encoding="utf-8") as polits:
         reader = csv.reader(polits)
         for row in reader:
             if keyword in row:
                 print(f'{keyword} Polski to {row[1]} {row[0]}')
 
 
-def count_sejm(keyword):
+def count_sejm(keyword="", party=""):
     count = 0
-    with open('sejm.csv', 'r') as sejm:
+    with open('sejm.csv', 'r', encoding="utf-8") as sejm:
         reader = csv.reader(sejm)
         for row in reader:
-            if keyword in row:
+            if keyword in row and party == "":
+                count += 1
+            elif keyword in row and party in row:
                 count += 1
     print(count)
     return count
 
 
-
-def check_hierarchy():
+def create_output():
     if "kto" in keywords:
         if "prezydent" in keywords:
             search_politicians("prezydent")
@@ -37,15 +38,54 @@ def check_hierarchy():
             for word in keywords:
                 if word in parse_from_input.clubs_keys.keys():
                     if word == "niezrzeszony":
-                        print(f"Posłów niezrzeszonych jest w sejmie {count_sejm(word)}")
+                        return f"Posłów niezrzeszonych jest w sejmie {count_sejm(word)}"
                     else:
-                        print(f"{word} ma w sejmie {count_sejm(word)} posłów")
+                        return f"{parse_from_input.clubs_keys.get(word)[1]} ma w sejmie {count_sejm(word)} posłów"
+            return f"posłów w sejmie jest {count_sejm()}"
+        if "prezydent" in keywords:
+            return "prezydent jest tylko jeden"
+        if "premier" in keywords:
+            return "premier jest tylko jeden"
+        if "m" in keywords:
+            for word in keywords:
+                if word in parse_from_input.clubs_keys.keys():
+                    return f"mężczyzn w partii {word} jest {count_sejm('m', word)}"
+            return f"mężczyzn jest w sejmie {count_sejm('m')}"
     if "ile" in keywords:
-        pass
+        if "k" in keywords:
+            for word in keywords:
+                if word in parse_from_input.clubs_keys.keys():
+                    return f"kobiet w partii {word} jest {count_sejm('k', word)}"
+            return f"kobiet jest w sejmie {count_sejm('k')}"
+    if "w" in keywords[0]:
+        list_of_lastnames = []
+        list_of_firstnames = []
+        club = []
+        for name in parse_from_input.split_usr_input[1:]:
+            with open('sejm.csv', 'r', encoding="utf-8") as sejm:
+                sejm = csv.reader(sejm)
+                for row in sejm:
+                    if name in row[0]:
+                        list_of_lastnames.append(name)
+                        club.append(row[2])
+                    if name in row[1]:
+                        club.append(row[2])
+                        list_of_firstnames.append(name)
 
+        if len(list_of_lastnames) > 1 and len(list_of_firstnames) == 0:
+            return "Więcej niż jedna osoba się tak nazywa"
+        elif len(list_of_lastnames) == 0 and len(list_of_firstnames) > 1:
+            return "Więcej niż jedna osoba ma tak na imię"
+        elif len(list_of_lastnames) == 1 and len(list_of_firstnames) == 0:
+            return f"{list_of_lastnames[0]} jest w partii {club[0]}"
+        elif len(list_of_firstnames) == 1 and len(list_of_lastnames) == 0:
+            return f"{list_of_firstnames[0]} jest w partii {club[0]}"
+        elif len(list_of_firstnames) == 0 and len(list_of_lastnames) == 0:
+            return "w żadnej partii nie ma takiej osoby"
+        else:
+            return f"{list_of_firstnames[0]} {list_of_lastnames[0]} jest w partii {club[0]}"
 
-check_hierarchy()
+    #TODO: upewnić się, że do polityka będzie dobra partia
 
-
-
-# output_message = choose_engine(keywords)
+output_message = create_output()
+print(output_message)
